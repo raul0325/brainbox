01c1a0df52d70a8327c9438edb6a9d6f8c3c8b09
raul0325 <raul0325@outlook.com>
2026-08-11 20:46:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "brainbox-v1";
+const CACHE_NAME = "brainbox-v2";
 const SHELL = ["./", "index.html", "style.css", "app.js", "manifest.json"];
 
 self.addEventListener("install", (e) => {
